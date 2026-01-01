2d5f80078d1db8676c1223989f52a47ee65f49a3
Shishir707 <mirshr8@gmail.com>
2026-01-01 09:37:20 +0600

    implement Realtime Database

--- lib/live_app.dart
+++ lib/live_app.dart
@@ -13,22 +13,22 @@ class _LiveAppState extends State<LiveApp> {
   final List<FootballMatch> _footballMatches = [];
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  @override
-  initState() {
-    super.initState();
-    _fetchDatabase();
-  }
-
-  Future<void> _fetchDatabase() async {
-    _footballMatches.clear();
-    QuerySnapshot<Map<String, dynamic>> snapshot = await _firestore
-        .collection('Football')
-        .get();
-    for (QueryDocumentSnapshot<Map<String, dynamic>> doc in snapshot.docs) {
-      _footballMatches.add(FootballMatch.fromJson(doc.data()));
-    }
-    setState(() {});
-  }
+  // @override
+  // initState() {
+  //   super.initState();
+  //   _fetchDatabase();
+  // }
+  //
+  // Future<void> _fetchDatabase() async {
+  //   _footballMatches.clear();
+  //   QuerySnapshot<Map<String, dynamic>> snapshot = await _firestore
+  //       .collection('Football')
+  //       .get();
+  //   for (QueryDocumentSnapshot<Map<String, dynamic>> doc in snapshot.docs) {
+  //     _footballMatches.add(FootballMatch.fromJson(doc.data()));
+  //   }
+  //   setState(() {});
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -41,34 +41,60 @@ class _LiveAppState extends State<LiveApp> {
         centerTitle: true,
         backgroundColor: Colors.purpleAccent,
       ),
-      body: ListView.separated(
-        itemCount: _footballMatches.length,
-        itemBuilder: (context, index) {
-          final match = _footballMatches[index];
-          return ListTile(
-            leading: CircleAvatar(
-              radius: 10,
-              backgroundColor: match.isRunning ? Colors.red : Colors.grey,
-            ),
-            title: Text(
-              "${match.team1Name} VS ${match.team2Name}",
-              style: TextTheme.of(context).titleLarge,
-            ),
-            subtitle: Text(
-              match.winnerTeam.isEmpty
-                  ? "Winner Team : Pending..."
-                  : "Winner Team : ${match.winnerTeam}",
-            ),
-            trailing: Text(
-              "${match.team1Score}:${match.team2Score}",
-              style: TextTheme.of(context).titleLarge,
-            ),
-          );
-        },
-        separatorBuilder: (context, index) {
-          return Divider();
+      body: StreamBuilder(
+        stream: _firestore.collection('Football').snapshots(),
+        builder: (context, asyncSnapshot) {
+          if (asyncSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (asyncSnapshot.hasError) {
+            return Center(child: Text("${asyncSnapshot.hasError}"));
+          }
+
+          if (asyncSnapshot.hasData) {
+            _footballMatches.clear();
+            for (QueryDocumentSnapshot<Map<String, dynamic>> doc
+                in asyncSnapshot.data!.docs) {
+              _footballMatches.add(FootballMatch.fromJson(doc.data()));
+            }
+            return uiResult();
+          }
+
+          return SizedBox();
         },
       ),
+    );
+  }
+
+  ListView uiResult() {
+    return ListView.separated(
+      itemCount: _footballMatches.length,
+      itemBuilder: (context, index) {
+        final match = _footballMatches[index];
+        return ListTile(
+          leading: CircleAvatar(
+            radius: 10,
+            backgroundColor: match.isRunning ? Colors.red : Colors.grey,
+          ),
+          title: Text(
+            "${match.team1Name} VS ${match.team2Name}",
+            style: TextTheme.of(context).titleLarge,
+          ),
+          subtitle: Text(
+            match.winnerTeam.isEmpty
+                ? "Winner Team : Pending..."
+                : "Winner Team : ${match.winnerTeam}",
+          ),
+          trailing: Text(
+            "${match.team1Score}:${match.team2Score}",
+            style: TextTheme.of(context).titleLarge,
+          ),
+        );
+      },
+      separatorBuilder: (context, index) {
+        return Divider();
+      },
     );
   }
 }
